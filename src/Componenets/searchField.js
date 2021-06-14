8333d9fcf1fee6c5766858f91678238723c87d04
Karimu Mohammed <Karimu.mohammed@gmail.com>
2021-06-14 09:42:01 -0400

    Subject: Add handleSubmit function in search field with proper api key

--- src/Componenets/searchField.js
+++ src/Componenets/searchField.js
@@ -1,8 +1,25 @@
 
+const handleSubmit = (event) => {
+
+    const url = 'http://api.giphy.com/v1/gifs/search';
+    const API_KEY = 'zv3vBNsGF6xI3nmtHkcBBd22oClKsEQr';
+    const fetchSearch = fetch(`${url}?api_key=${API_KEY}&q=${event.value}}`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(myJson) {
+            console.log(myJson);
+        });
+    const searchResults = fetchSearch.data.data;
+}
+
+
+
+
 function Search(props) {
   return (
     <div >
-        <form >
+        <form onSubmit={props.handleSubmit}>
             <input
                 type="text"
                 value={props.query}
